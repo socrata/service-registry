7be48cbf9eb7e8a38486448ec8c2bcef1700565c
Erik Thorne <erik.thorne@tylertech.com>
2025-01-06 14:11:13 -0800

    add github org provider, webhook, events
    * add the github org provider
    * hook up the github events backend
    * configure a webhook in the github organization: socrata
      * NOTE: this is currently using an ngrok url, as this is all currently
        being tested
      * when we deploy this, that webhook configuration will need to be
        changed (in the github.com UI)
    * user will only be able to log in if they have an atlassian account, a
      github account and one of the following condiditions is met:
      * a matching username between atlassian and github
      * if they put their tylertech email in their github account
      * email local part (e.g. stuff before the @) github username
    * config cleanup
      * most notably, I commented out some prod config I'm not yet using
        (but might later)
    * miscellaneous TODOs scattered across the code

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,6 +1,10 @@
 import { createBackend } from '@backstage/backend-defaults';
+import { eventsModuleGithubEventRouter } from '@backstage/plugin-events-backend-module-github/alpha';
+import { eventsModuleGithubWebhook } from '@backstage/plugin-events-backend-module-github/alpha';
 
 const backend = createBackend();
+
+// TODO - do we still need the scaffolders?
 
 backend.add(import('@backstage/plugin-app-backend'));
 backend.add(import('@backstage/plugin-proxy-backend'));
@@ -19,6 +23,11 @@ backend.add(import('@backstage/plugin-events-backend'))
 // catalog plugin
 backend.add(import('@backstage/plugin-catalog-backend'));
 backend.add(import('@backstage/plugin-catalog-backend-module-scaffolder-entity-model'));
+
+// github org provider
+backend.add(eventsModuleGithubEventRouter);
+backend.add(eventsModuleGithubWebhook);
+backend.add(import('@backstage/plugin-catalog-backend-module-github-org'));
 
 // See https://backstage.io/docs/features/software-catalog/configuration#subscribing-to-catalog-errors
 backend.add(import('@backstage/plugin-catalog-backend-module-logs'));
